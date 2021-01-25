c145251e7f7a13fbcbb7c5987652222d78f6e084
김기빈 <kibin3846@gmail.com>
2021-01-25 16:59:35 +0900

    404페이지 오류로 인한 수정

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from "react-dom";
 import Root from "./Root";
 import * as serviceWorker from "./serviceWorker";
 import { store } from "common/config";
+import Main from "main/Main";
+import Contact from "contact/Contact";
+import Header from "common/Header";
+import Footer from "common/Footer";
+import NotFound from "notfound/NotFound";
 
 ReactDOM.render(<Root store={store} />, document.getElementById("root"));
 
@@ -10,3 +15,5 @@ ReactDOM.render(<Root store={store} />, document.getElementById("root"));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+export { Main, Contact, Header, Footer, NotFound };
